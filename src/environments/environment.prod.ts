3fc248fffcf0edb9177e850fbfd93b193bfcb216
Nitin-Tankup <nitin@tankup.co.in>
2022-06-23 18:01:55 +0530

    staging url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_URL: 'http://3.109.101.84:3300/',
-  // HOME_URL: 'http://crictour-9e79c.web.app',
+  HOME_URL: 'http://staging.tankup.co.in',
 };
